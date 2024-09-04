6624c918cb66ead4d81319dec1fe07134570f6b1
Andrii Konovalov <andy.smokk@gmail.com>
2024-09-04 13:08:01 +0200

    styled header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex first-line:items-center justify-between">
+        <Link href="/" className="flex">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={38}
+            height={38}
+          />
+          <p className="h2-bold ml-1">Event Radar</p>
+        </Link>
+
+        <div className="flex w-32 justify-end gap-3"></div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
