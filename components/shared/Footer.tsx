4890f2ad5af3c2bd8e03ea438d6f280bb42b6520
Andrii Konovalov <andy.smokk@gmail.com>
2024-09-11 20:30:32 +0200

    finished footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="wrapper flex flex-col flex-center flex-between gap-4 sm:flex-row">
+        <Link href="/" className="flex">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={38}
+            height={38}
+          />
+          <p className="h5-bold ml-1">Event Radar</p>
+        </Link>
+        <p>2023 Event Radar. All Rights reserved.</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
