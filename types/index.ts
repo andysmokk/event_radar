ff7147463f9f94f8a173c4816f66a14ec266a57b
Andrii Konovalov <andy.smokk@gmail.com>
2024-09-20 21:28:51 +0200

    added CreateEventParams type

--- types/index.ts
+++ types/index.ts
@@ -13,3 +13,20 @@ export type UpdateUserParams = {
   username: string;
   photo: string;
 };
+
+export type CreateEventParams = {
+  userId: string;
+  event: {
+    title: string;
+    description: string;
+    location: string;
+    imageUrl: string;
+    startDateTime: Date;
+    endDateTime: Date;
+    categoryId: string;
+    price: string;
+    isFree: boolean;
+    url: string;
+  };
+  path: string;
+};
