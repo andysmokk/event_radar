ab5303e94c65448059facc760a787980179215c7
Andrii Konovalov <andy.smokk@gmail.com>
2024-09-23 17:25:18 +0200

    updated app's name

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -12,7 +12,7 @@ export const connectToDatabase = async () => {
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URI, {
-      dbName: "Event Radar",
+      dbName: "event_radar",
       bufferCommands: false,
     });
 
