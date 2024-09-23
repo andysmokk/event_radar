8332381b0e8b4b373d1c9d59a146503d508aad7b
Andrii Konovalov <andy.smokk@gmail.com>
2024-09-23 17:58:24 +0200

    test3

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -10,17 +10,29 @@ import { handleError } from "@/lib/utils";
 
 import { CreateUserParams, UpdateUserParams } from "@/types";
 
-export const createUser = async (user: CreateUserParams) => {
+export async function createUser(user: CreateUserParams) {
   try {
     await connectToDatabase();
 
     const newUser = await User.create(user);
-
     return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
     handleError(error);
   }
-};
+}
+
+export async function getUserById(userId: string) {
+  try {
+    await connectToDatabase();
+
+    const user = await User.findById(userId);
+
+    if (!user) throw new Error("User not found");
+    return JSON.parse(JSON.stringify(user));
+  } catch (error) {
+    handleError(error);
+  }
+}
 
 export async function updateUser(clerkId: string, user: UpdateUserParams) {
   try {
--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,4 @@
+// ====== USER PARAMS
 export type CreateUserParams = {
   clerkId: string;
   firstName: string;
@@ -14,6 +15,7 @@ export type UpdateUserParams = {
   photo: string;
 };
 
+// ====== EVENT PARAMS
 export type CreateEventParams = {
   userId: string;
   event: {
@@ -29,4 +31,119 @@ export type CreateEventParams = {
     url: string;
   };
   path: string;
+};
+
+export type UpdateEventParams = {
+  userId: string;
+  event: {
+    _id: string;
+    title: string;
+    imageUrl: string;
+    description: string;
+    location: string;
+    startDateTime: Date;
+    endDateTime: Date;
+    categoryId: string;
+    price: string;
+    isFree: boolean;
+    url: string;
+  };
+  path: string;
+};
+
+export type DeleteEventParams = {
+  eventId: string;
+  path: string;
+};
+
+export type GetAllEventsParams = {
+  query: string;
+  category: string;
+  limit: number;
+  page: number;
+};
+
+export type GetEventsByUserParams = {
+  userId: string;
+  limit?: number;
+  page: number;
+};
+
+export type GetRelatedEventsByCategoryParams = {
+  categoryId: string;
+  eventId: string;
+  limit?: number;
+  page: number | string;
+};
+
+export type Event = {
+  _id: string;
+  title: string;
+  description: string;
+  price: string;
+  isFree: boolean;
+  imageUrl: string;
+  location: string;
+  startDateTime: Date;
+  endDateTime: Date;
+  url: string;
+  organizer: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  };
+  category: {
+    _id: string;
+    name: string;
+  };
+};
+
+// ====== CATEGORY PARAMS
+export type CreateCategoryParams = {
+  categoryName: string;
+};
+
+// ====== ORDER PARAMS
+export type CheckoutOrderParams = {
+  eventTitle: string;
+  eventId: string;
+  price: string;
+  isFree: boolean;
+  buyerId: string;
+};
+
+export type CreateOrderParams = {
+  stripeId: string;
+  eventId: string;
+  buyerId: string;
+  totalAmount: string;
+  createdAt: Date;
+};
+
+export type GetOrdersByEventParams = {
+  eventId: string;
+  searchString: string;
+};
+
+export type GetOrdersByUserParams = {
+  userId: string | null;
+  limit?: number;
+  page: string | number | null;
+};
+
+// ====== URL QUERY PARAMS
+export type UrlQueryParams = {
+  params: string;
+  key: string;
+  value: string | null;
+};
+
+export type RemoveUrlQueryParams = {
+  params: string;
+  keysToRemove: string[];
+};
+
+export type SearchParamProps = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
 };
