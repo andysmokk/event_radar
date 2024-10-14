3d273a7801cabcef0850937e3ad37c9d00415c86
Andrii Konovalov <andy.smokk@gmail.com>
2024-10-14 17:16:38 +0200

    +

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import Link from "next/link";
 
-const Card = () => {
-  return <div>Card</div>;
+import { IEvent } from "@/lib/database/models/event.model";
+
+type CardProps = {
+  event: IEvent;
+  hasOrderLink?: boolean;
+  hidePrice?: boolean;
+};
+
+const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
+  return (
+    <div
+      className="group relative flex min-h-[380px] w-full max-w-[400px]
+     flex-col overflow-hidden bg-white shadow-md transition-all
+      hover:shadow-lg md:min-h-[438px]"
+    >
+      <Link
+        href={`/events/${event._id}`}
+        style={{ backgroundImage: `url(${event.imageUrl})` }}
+        className="flex-center flex-grow bg-gray-50 bg-cover bg-center text-grey-500"
+      ></Link>
+    </div>
+  );
 };
 
 export default Card;
