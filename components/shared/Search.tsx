2970e675d63a0e4b530dcc8c0194a364a3f78453
Andrii Konovalov <andy.smokk@gmail.com>
2024-11-08 13:39:07 +0100

    implemented Search component

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -53,7 +53,7 @@ const Search = ({
       />
       <Input
         type="text"
-        placeholder="Search"
+        placeholder={placeholder}
         onChange={(e) => setQuery(e.target.value)}
         className="p-regular-16 border-0 bg-grey-50 outline-offset-0
          placeholder:text-grey-500 focus:border-0 focus-visible:ring-0
