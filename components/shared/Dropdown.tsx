4921b08c3390e0717b715770e19b34e81015ad7a
Andrii Konovalov <andy.smokk@gmail.com>
2024-11-14 20:46:10 +0100

    fixed Dropdown

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -59,23 +59,12 @@ const Dropdown = ({ onChangeHandler, value }: DropdownProps) => {
         <SelectValue placeholder="Category" />
       </SelectTrigger>
       <SelectContent>
-        {categories.length > 0 &&
-          categories.map((category) => (
-            <SelectItem
-              key={category._id}
-              value={category._id}
-              className="select-item p-regular-14"
-            >
-              {category.name}
-            </SelectItem>
-          ))}
-
         <AlertDialog>
           <AlertDialogTrigger
             className="p-medium-14 flex w-full rounded-sm py-3
            pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500"
           >
-            Open
+            Create New Category
           </AlertDialogTrigger>
           <AlertDialogContent className="bg-white">
             <AlertDialogHeader>
@@ -94,11 +83,52 @@ const Dropdown = ({ onChangeHandler, value }: DropdownProps) => {
               <AlertDialogAction
                 onClick={() => startTransition(handleAddCategory)}
               >
-                Add new Category
+                Add New Category
               </AlertDialogAction>
             </AlertDialogFooter>
           </AlertDialogContent>
         </AlertDialog>
+
+        {categories.length > 0 &&
+          categories.map((category) => (
+            <SelectItem
+              key={category._id}
+              value={category._id}
+              className="select-item p-regular-14"
+            >
+              {category.name}
+            </SelectItem>
+          ))}
+
+        {/* <AlertDialog>
+          <AlertDialogTrigger
+            className="p-medium-14 flex w-full rounded-sm py-3
+           pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500"
+          >
+            Create New Category
+          </AlertDialogTrigger>
+          <AlertDialogContent className="bg-white">
+            <AlertDialogHeader>
+              <AlertDialogTitle>New Category</AlertDialogTitle>
+              <AlertDialogDescription>
+                <Input
+                  type="text"
+                  placeholder="Category name"
+                  className="input-field mt-3"
+                  onChange={(e) => setNewCategory(e.target.value)}
+                />
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction
+                onClick={() => startTransition(handleAddCategory)}
+              >
+                Add New Category
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog> */}
       </SelectContent>
     </Select>
   );
