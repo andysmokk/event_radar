09f61910763ecda0be453c48583307099d3110fe
Andrii Konovalov <andy.smokk@gmail.com>
2024-11-21 16:04:14 +0100

    added onClick to Pagination component

--- components/shared/Pagination.tsx
+++ components/shared/Pagination.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter, useSearchParams } from "next/navigation";
 import { Button } from "../ui/button";
+import { formUrlQuery } from "@/lib/utils";
 
 type PaginationProps = {
   urlParamName?: string;
@@ -13,7 +14,17 @@ const Pagination = ({ urlParamName, page, totalPages }: PaginationProps) => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  const onClick = (btnType: string) => {};
+  const onClick = (btnType: string) => {
+    const pageValue = btnType === "next" ? Number(page) + 1 : Number(page) - 1;
+
+    const newUrl = formUrlQuery({
+      params: searchParams.toString(),
+      key: urlParamName || "page",
+      value: pageValue.toString(),
+    });
+
+    router.push(newUrl, { scroll: false });
+  };
 
   return (
     <div className="flex gap-2">
